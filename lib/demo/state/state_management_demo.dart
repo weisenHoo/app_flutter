ae8da80a0c9b82554477588eb57671ad13584a3c
weisen <306429737@qq.com>
2019-05-28 18:06:32 +0800

    '使用ScopedModel传递数据'

--- lib/demo/state/state_management_demo.dart
+++ lib/demo/state/state_management_demo.dart
@@ -1,73 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class StateManagementDemo extends StatefulWidget {
-  @override
-  _StateManagementDemoState createState() => _StateManagementDemoState();
-}
-
-class _StateManagementDemoState extends State<StateManagementDemo> {
-  int _count = 0;
-
-  void _increaseCount() {
-    setState(() {
-      _count += 1;
-    });
-    print(_count);
-  }
+class StateManagementDemo extends StatelessWidget {
+  // @override
+  // _StateManagementDemoState createState() => _StateManagementDemoState();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StateManagementDemo'),
-        elevation: 0.0,
-      ),
-      body: CounterWrapper(
-        count: _count,
-        increaseCount: _increaseCount,
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: _increaseCount,
+    return ScopedModel(
+      model: CounterModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('StateManagementDemo'),
+          elevation: 0.0,
+        ),
+        body: CounterWrapper(),
+        floatingActionButton: ScopedModelDescendant<CounterModel>(
+          rebuildOnChange: false,
+          builder: (context, _, model) => FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: model.increaseCount,
+              ),
+        ),
       ),
     );
   }
 }
 
+// class _StateManagementDemoState extends State<StateManagementDemo> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScopedModel(
+//       model: CounterModel(),
+//       child: Scaffold(
+//         appBar: AppBar(
+//           title: Text('StateManagementDemo'),
+//           elevation: 0.0,
+//         ),
+//         body: CounterWrapper(),
+//         floatingActionButton: FloatingActionButton(
+//           child: Icon(Icons.add),
+//           onPressed: _increaseCount,
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 class CounterWrapper extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount;
-
-  CounterWrapper({
-    this.count,
-    this.increaseCount,
-  });
-
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Counter(
-        count: count,
-        increaseCount: increaseCount,
-      ),
+      child: Counter(),
     );
   }
 }
 
 class Counter extends StatelessWidget {
-  final int count;
-  final VoidCallback increaseCount;
-
-  Counter({
-    this.count,
-    this.increaseCount,
-  });
-
   @override
   Widget build(BuildContext context) {
-    return ActionChip(
-      label: Text('$count'),
-      onPressed: increaseCount,
+    return ScopedModelDescendant<CounterModel>(
+      builder: (context, _, model) => ActionChip(
+            label: Text('${model.count}'),
+            onPressed: model.increaseCount,
+          ),
     );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final int count;
+  final VoidCallback increaseCount;
+  final Widget child;
+
+  CounterProvider({
+    this.count,
+    this.increaseCount,
+    this.child,
+  }) : super(child: child);
+
+  static CounterProvider of(BuildContext context) =>
+      context.inheritFromWidgetOfExactType(CounterProvider);
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    return true;
+  }
+}
+
+class CounterModel extends Model {
+  int _count = 0;
+  int get count => _count;
+
+  void increaseCount() {
+    _count += 1;
+    notifyListeners();
   }
 }
