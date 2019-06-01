61e8cf51649aa586ae3d0360ac99c5e0172bd425
“weisen” <306429737@qq.com>
2019-06-01 16:20:20 +0800

    bloc

--- lib/demo/bloc/bloc_demo.dart
+++ lib/demo/bloc/bloc_demo.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import './counter_bloc_demo.dart';
 
 class BlocDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BlocDemo'),
-        elevation: 0.0,
+    return CounterProvider(
+      bloc: CounterBloc(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('BlocDemo'),
+          elevation: 0.0,
+        ),
+        body: CounterHome(),
+        floatingActionButton: CounterActionButton(),
       ),
     );
   }
